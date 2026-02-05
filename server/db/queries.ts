d87424332a580a272c066e3278e09c4bd19b5079
godccino <supitchaklanpradit@outlook.com>
2026-02-05 10:24:56 +0700

    updates

--- server/db/queries.ts
+++ server/db/queries.ts
@@ -1,12 +1,15 @@
-import { desc, eq } from 'drizzle-orm';
-import { db } from './db';
-import { todos } from './schema'
+import { desc, eq } from "drizzle-orm";
+import { db } from "./db";
+import { todos } from "./schema";
 
 export const getTodos = async () => {
-  return await db.select().from(todos).orderBy(desc(todos.createdAt))
-}
+  return await db.select().from(todos).orderBy(desc(todos.createdAt));
+};
 
-
-export const getTodosByUserId = async (userId: string ) => {
-  return await db.select().from(todos).where(eq(todos.userId, userId)).orderBy(desc(todos.createdAt))
-}
+export const getTodosByUserId = async (userId: string) => {
+  return await db
+    .select()
+    .from(todos)
+    .where(eq(todos.userId, userId))
+    .orderBy(desc(todos.createdAt));
+};
