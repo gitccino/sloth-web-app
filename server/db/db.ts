12d5bf15a7a728687fb526be13207bc0e4e72782
godccino <supitchaklanpradit@outlook.com>
2026-02-18 00:03:52 +0700

    Configures database connection using Neon serverless

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,8 +1,48 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
+import {
+  // drizzle as neonDrizzle,
+  NeonDatabase,
+} from "drizzle-orm/neon-serverless";
+import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
+// import type { NeonHttpDatabase } from "drizzle-orm/neon-http";
 import { Pool } from "pg";
+import { Pool as neonPool, neon, neonConfig } from "@neondatabase/serverless";
+import * as ws from "ws";
+import * as schema from "./schema";
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+neonConfig.webSocketConstructor = ws;
 
-export const db = drizzle({ client: pool });
+let dbClient:
+  | ReturnType<typeof drizzle<typeof schema>>
+  | ReturnType<typeof neonDrizzle<typeof schema>>;
+// let dbClient:
+//   | (NodePgDatabase<typeof schema> & {
+//       $client: Pool;
+//     })
+//   | NeonDatabase<typeof schema>;
+
+const getDbConnection = () => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in environment variables");
+  }
+
+  if (!dbClient && process.env.APP_ENV === "production") {
+    // NEON
+    // const neonPoolInstance = new neonPool({
+    //   connectionString: process.env.DATABASE_URL!,
+    // });
+    const neonPoolInstance = neon(process.env.DATABASE_URL!);
+    dbClient = neonDrizzle({ client: neonPoolInstance });
+  }
+
+  if (!dbClient && process.env.APP_ENV === "development") {
+    // PG
+    dbClient = drizzle({ client: pool });
+  }
+
+  return dbClient;
+};
+
+// For development
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = getDbConnection();
